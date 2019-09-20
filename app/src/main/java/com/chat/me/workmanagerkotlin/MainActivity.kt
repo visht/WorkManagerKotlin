f5f82c239047dae6297f50cd1be845843e428621
vishal <playvishal13@gmail.com>
2019-09-20 20:04:56 +0530

    An update:-
    1:- Onrotation tested data is not loosing
    2:- Fragment is attached for data observing

--- app/src/main/java/com/chat/me/workmanagerkotlin/MainActivity.kt
+++ app/src/main/java/com/chat/me/workmanagerkotlin/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.chat.me.workmanagerkotlin
 
 import android.arch.lifecycle.Observer
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentTransaction
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -12,7 +15,10 @@ import androidx.work.WorkInfo
 import androidx.work.WorkManager
 import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TestFragment.OnFragmentInteractionListener{
+    override fun onFragmentInteraction(uri: Uri) {
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,6 +46,12 @@ class MainActivity : AppCompatActivity() {
                 var value: TextView = findViewById(R.id.textView)
                 workMgr.getWorkInfosByTagLiveData("workvisht").observe(this@MainActivity, object : Observer<MutableList<WorkInfo>> {
                     override fun onChanged(t: MutableList<WorkInfo>?) {
+                        var tf: TestFragment = TestFragment.newInstance("${t!!.get(0).id}", t!!.get(0).state.name)
+                        var fm: FragmentManager = supportFragmentManager
+                        var ft: FragmentTransaction = fm.beginTransaction()
+                        ft.add(R.id.fragment, tf)
+                        ft.addToBackStack("TAG A")
+                        ft.commit()
                         value.setText(t?.get(0)?.state?.name)
                         button2.setText("isFinished ${t?.get(0)?.state?.isFinished}")
                         button3.setText(t?.get(0)?.outputData.toString())
